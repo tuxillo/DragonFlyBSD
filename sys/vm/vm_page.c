7bf547f8d6cf5d8c4ab1f649ab757a96a2f71bb5
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-26 18:43:34 +0100

    vm_page: increase debug trace limit from 5 to 10 iterations
    The hang was observed after exactly 5 successful page additions,
    right when our debug output stopped.  Increase the limit to 10
    to see if the 6th iteration (pa=0x42005000) is entered and where
    it hangs.

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -248,13 +248,14 @@ vm_add_new_page(vm_paddr_t pa, int *badcountp)
 	vm_page_t m;
 	static int call_count = 0;
 
-	if (call_count < 5)
+	/* Print entry for first 10 calls to catch the hang point */
+	if (call_count < 10)
 		kprintf("vm_add_new_page[%d]: pa=%#jx\n",
 			call_count, (uintmax_t)pa);
 
 	m = PHYS_TO_VM_PAGE(pa);
 
-	if (call_count < 5)
+	if (call_count < 10)
 		kprintf("  [%d] m=%p\n", call_count, m);
 
 	/*
@@ -302,7 +303,7 @@ vm_add_new_page(vm_paddr_t pa, int *badcountp)
 	m->pc ^= ((pa >> PAGE_SHIFT) / (PQ_L2_SIZE * PQ_L2_SIZE));
 	m->pc &= PQ_L2_MASK;
 
-	if (call_count < 5)
+	if (call_count < 10)
 		kprintf("  [%d] queue=%d pc=%d\n", call_count,
 			m->pc + PQ_FREE, m->pc);
 
@@ -327,7 +328,7 @@ vm_add_new_page(vm_paddr_t pa, int *badcountp)
 		m->wire_count = 1;
 		vmstats.v_wire_count++;
 		alist_free(&vm_contig_alist, pa >> PAGE_SHIFT, 1);
-		if (call_count < 5)
+		if (call_count < 10)
 			kprintf("  [%d] DMA reserved path, returning\n",
 				call_count);
 		call_count++;
@@ -345,10 +346,10 @@ vm_add_new_page(vm_paddr_t pa, int *badcountp)
 	vmstats.v_page_count++;
 	vmstats.v_free_count++;
 	vpq = &vm_page_queues[m->queue];
-	if (call_count < 5)
+	if (call_count < 10)
 		kprintf("  [%d] calling TAILQ_INSERT_HEAD\n", call_count);
 	TAILQ_INSERT_HEAD(&vpq->pl, m, pageq);
-	if (call_count < 5)
+	if (call_count < 10)
 		kprintf("  [%d] TAILQ_INSERT_HEAD done\n", call_count);
 	++vpq->lcnt;
 	call_count++;
