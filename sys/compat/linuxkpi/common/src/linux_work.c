b628207f26a5708e258e18d65b39dcab1e21fc94
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-02-03 10:14:16 +0100

    testcases

--- sys/compat/linuxkpi/common/src/linux_work.c
+++ sys/compat/linuxkpi/common/src/linux_work.c
@@ -519,7 +519,8 @@ linux_cancel_timer(struct delayed_work *dwork, bool drain)
 	bool cancelled;
 
 	mtx_lock(&dwork->timer.mtx);
-	cancelled = (callout_stop(&dwork->timer.callout) == 1);
+	cancelled = (callout_pending(&dwork->timer.callout) != 0);
+	callout_stop(&dwork->timer.callout);
 	mtx_unlock(&dwork->timer.mtx);
 
 	/* check if we should drain */
