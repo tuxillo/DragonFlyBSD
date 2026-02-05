12be6f36115b964d37cc095048ec05734be63014
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-02-05 18:45:09 +0100

    drm-kmod fixes

--- sys/compat/linuxkpi/common/include/linux/kernel.h
+++ sys/compat/linuxkpi/common/include/linux/kernel.h
@@ -58,6 +58,10 @@
 #include <linux/types.h>
 #include <linux/typecheck.h>
 #include <linux/jiffies.h>
+
+#ifdef __DragonFly__
+#define	snprintf	ksnprintf
+#endif
 #include <linux/log2.h>
 #include <linux/kconfig.h>
 
