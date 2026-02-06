450f0484b0d516f8f6b98f7885f26a2e03f8e4b7
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-02-06 15:13:00 +0100

    drm-kmod fixes

--- sys/compat/linuxkpi/common/include/linux/kernel.h
+++ sys/compat/linuxkpi/common/include/linux/kernel.h
@@ -306,13 +306,37 @@ linux_ratelimited(linux_ratelimit_t *rl)
 /*
  * This returns a constant expression while determining if an argument is
  * a constant expression, most importantly without evaluating the argument.
+ * Glory to Martin Uecker <Martin.Uecker@med.uni-goettingen.de>
  *
- * Use __builtin_constant_p() which works correctly with GCC and avoids
- * -Wpointer-arith warnings from the sizeof(void) trick used in other
- * implementations.
+ * Details:
+ * - sizeof() return an integer constant expression, and does not evaluate
+ *   the value of its operand; it only examines the type of its operand.
+ * - The results of comparing two integer constant expressions is also
+ *   an integer constant expression.
+ * - (long)(x) is used to avoid warnings about 64-bit types on 32-bit
+ *   architectures.
+ * - The C Standard defines "null pointer constant", "(void *)0", as
+ *   distinct from other void pointers.
+ * - If (x) is an integer constant expression, then the "* 0l" resolves
+ *   it into an integer constant expression of value 0. Since it is cast to
+ *   "void *", this makes the second operand a null pointer constant.
+ * - If (x) is not an integer constant expression, then the second operand
+ *   resolves to a void pointer (but not a null pointer constant: the value
+ *   is not an integer constant 0).
+ * - The conditional operator's third operand, "(int *)8", is an object
+ *   pointer (to type "int").
+ * - The behavior (including the return type) of the conditional operator
+ *   depends on the kind of expressions given for the second and third operands:
+ *   - When one operand is a null pointer constant and the other is an object
+ *     pointer, the conditional operator returns the type of the object pointer
+ *     operand (i.e. "int *"). sizeof(*(int *)) == sizeof(int) == 4.
+ *   - When one operand is a void pointer and the other is an object pointer,
+ *     the conditional operator returns "void *". sizeof(*(void *)) == 1.
+ * - The equality comparison to sizeof(int) therefore returns 1 if x was a
+ *   constant expression, 0 otherwise.
  */
 #define	__is_constexpr(x) \
-	__builtin_constant_p(x)
+	(sizeof(int) == sizeof(*(8 ? ((void *)((long)(x) * 0l)) : (int *)8)))
 
 /*
  * The is_signed() macro below returns true if the passed data type is
