2d6c9b640bec348f4f267c17fb326a1cb45505b7
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-02-09 00:23:30 +0100

    Increase kernel stack size (UPAGES) from 4 to 8 pages
    The i915 DRM driver has very deep call chains during probe that
    exhaust the default 16KB kernel stack. Increasing to 32KB (8 pages)
    provides sufficient stack space.
    
    Note: Stack guard pages are already enabled via COWF_IS_KSTACK in
    vm_kern.c line 227, which sets gstart = PAGE_SIZE to create a
    protected guard page below the kernel stack.

--- sys/cpu/x86_64/include/param.h
+++ sys/cpu/x86_64/include/param.h
@@ -123,7 +123,7 @@
 #define MAXDUMPPGS	(MAXPHYS/PAGE_SIZE)
 
 #define IOPAGES		2		/* pages of i/o permission bitmap */
-#define UPAGES		4		/* pages of u-area */
+#define UPAGES		8		/* pages of u-area - increased for LinuxKPI/DRM drivers */
 
 /*
  * Ceiling on amount of swblock kva space, can be changed via
